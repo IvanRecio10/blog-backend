5a702b44d04d62f232a7e6d21d6221586f725192
IvanRecio10 <159625109+IvanRecio10@users.noreply.github.com>
2025-05-06 21:41:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const dotenv = require('dotenv');
 const postRoutes = require('./routes/postRoutes');
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use('/api/posts', postRoutes);
 
